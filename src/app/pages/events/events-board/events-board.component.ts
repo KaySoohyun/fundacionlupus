4adab852a995c2dc8eeeef699e3ed4cab25ea494
Julieta Wiesztort <julieta.raw@gmail.com>
2023-01-26 20:27:35 -0300

    pause events wip

--- src/app/pages/events/events-board/events-board.component.ts
+++ src/app/pages/events/events-board/events-board.component.ts
@@ -13,29 +13,23 @@ export class EventsBoardComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.firestore.collection('events').snapshotChanges()
-    .subscribe((items: any) => {
-      this.events = items.map((item: any) => {
-        const user: any = {
-          data: item.payload.doc.data(),
-          id: item.payload.doc.id,
-        }
-        return user;
-      })
-      this.events.sort(function (a, b) {
-        return b.data.index - a.data.index;
-      });
-      console.log(this.events)
-    }),
-    (error: any) => {
-      console.log(error)
-      // mostrar error
-    }
-  }
-
-  wait() {
-    console.log(this.events)
-    return this.events.length > 0;
+    // this.firestore.collection('events').snapshotChanges()
+    // .subscribe((items: any) => {
+    //   this.events = items.map((item: any) => {
+    //     const user: any = {
+    //       data: item.payload.doc.data(),
+    //       id: item.payload.doc.id,
+    //     }
+    //     return user;
+    //   })
+    //   this.events.sort(function (a, b) {
+    //     return b.data.index - a.data.index;
+    //   });
+    // }),
+    // (error: any) => {
+    //   console.log(error)
+    //   // mostrar error
+    // }
   }
 
 }
